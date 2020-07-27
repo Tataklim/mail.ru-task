15f8e5eb4ccb6c01db793e6db021aa984fe155e2
Tataklim <tatakaklim@gmail.com>
2020-07-27 17:03:23 +0300

    [ADD] deploy

---
+++ .out/precache-manifest.241c0d7355ddcbf7216cb8e1101ae676.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5be63db7f1409b8ff54b2f06d700472f",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.1de44f58b49a22faae98.bundle.js"
+  },
+  {
+    "url": "runtime~main.1de44f58b49a22faae98.bundle.js"
+  },
+  {
+    "revision": "a33174c8553cbfa35b0b",
+    "url": "static/css/main.bf98e957.chunk.css"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "01774ac88f3c13a260cc502328286549",
+    "url": "static/media/check_circle_outline-24px.01774ac8.svg"
+  },
+  {
+    "revision": "16793e816beb3255cd81812f58231bff",
+    "url": "static/media/delete-24px.16793e81.svg"
+  },
+  {
+    "url": "vendors~main.1de44f58b49a22faae98.bundle.js"
+  },
+  {
+    "url": "vendors~main.1de44f58b49a22faae98.bundle.js.LICENSE.txt"
+  }
+]);
